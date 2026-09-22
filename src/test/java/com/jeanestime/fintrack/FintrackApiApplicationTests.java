4e185a6f425148dad349877b86d81a46b1d77f5e
Jean Woodly Estime <jeanwoodlytime15@gmail.com>
2026-09-21 23:08:18 -0300

    chore: initialize Spring Boot project

---
+++ src/test/java/com/jeanestime/fintrack/FintrackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeanestime.fintrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintrackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
